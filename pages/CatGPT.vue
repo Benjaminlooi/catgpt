ce33d0de1932ec1fe250d2a7e131d71ab9997253
Benjamin Looi <benjaminlooi97@gmail.com>
2023-02-22 10:36:07 +0800

    Hide input border

--- pages/CatGPT.vue
+++ pages/CatGPT.vue
@@ -32,7 +32,7 @@
                 class="flex flex-col w-full py-2 flex-grow md:py-3 md:pl-4 relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]">
                 <textarea @keydown.enter="submitInput" v-model="input" tabindex="0"
                   style="max-height: 200px; height: 24px; overflow-y: hidden;" rows="1"
-                  class="m-0 w-full resize-none border-0 bg-transparent p-0 pl-2 pr-7 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:pl-0"></textarea>
+                  class="m-0 w-full resize-none border-0 bg-transparent p-0 pl-2 pr-7 focus:outline-0 focus:ring-0 focus-visible:ring-0 dark:bg-transparent md:pl-0"></textarea>
                 <button
                   class="absolute p-1 rounded-md text-gray-500 bottom-1.5 right-1 md:bottom-2.5 md:right-2 hover:bg-gray-100 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent">
                   <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round"
